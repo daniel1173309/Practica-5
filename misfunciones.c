1908b3bae74596f2b87589b2dda14fbe09ac76ff
daniel1173309 <125154087+daniel1173309@users.noreply.github.com>
2023-04-24 09:51:07 -0700

    Create misfunciones.c

---
+++ misfunciones.c
@@ -0,0 +1,11 @@
+int sumar_enteros(int num1, int num2){
+    int result=num1+num2;
+    return result;
+
+}
+
+
+int restar_enteros(int num1, int num2){
+    int result=num1-num2;
+    return result;
+}
